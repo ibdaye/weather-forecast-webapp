6ba45db57fa0219b04441b129d7eb32f14fb380a
ibibo seleye-fubara <ibibofubara@gmail.com>
2020-10-17 11:45:34 +0100

    made some changes to the about page

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,16 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'Loading...'
-messageTwo.textContent = ''
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
     const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    
     
     
     fetch('/weather?address=' + location).then((response)=>{
